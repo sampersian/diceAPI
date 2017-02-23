55a2a5c430d18e16cd16d03b80f2d4cdc1d3cb55
Samuel Persian <samuel.persian@gmail.com>
2017-02-23 12:13:42 -0700

    dX is now working. NdX is next.

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,6 @@ var router = express.Router();
 router.get('/:roll', function(req, res, next) {
   let box = {
     'entry': '',
-    'type': '',
     'format': '',
     'total': 0
   }
@@ -22,30 +21,35 @@ router.get('/:roll', function(req, res, next) {
     if (isXAnyPositiveNumber(roll)) {
       console.log(roll,"its a positive number with the length of",roll.length);
       console.log('returing literal d');
-      box.type = 'literal';
       box.format = 'd';
       box.total = Number(roll);
       finishRoll();
-    } else {
-      // If not, check if 'd' is in the string
-      if (roll.indexOf('d') === 0) {
+    } else { // If not, check if 'd' is in the string
+      if (roll.indexOf('d') === 0) {  // 1) if 'd' is the first item in the string:
         console.log('d is the first letter in the string');
         box.format = 'dString'
-      } else { // If 'd' is not in the string, return an error
+
+        let rightOfFirstD = roll.substring(1,roll.length);
+        console.log("right of first d:",rightOfFirstD);
+
+        if (isXAnyPositiveNumber(rightOfFirstD)) { // 1a) if the only thing after it is a positive number (X):    dX
+          box.format = 'dX';
+          box.total = rollOneDi(Number(rightOfFirstD))
+        } else {  // 1b) if the thing after it is not a positve number
+
+        }
+      } else if (roll.indexOf('d') > 0){ // If 'd' is not in the string, return an error
         console.log('d is NOT the first letter in the string');
-        box.format = '?'
+        box.format = 'stringd'
+      } else {
+        console.log('d is NOT in the string');
+        box.format = 'ERR';
+        box.total = 'ERR';
       }
       finishRoll()
     }
 
 
-
-    // If 'd' is in the string:
-      // 1) if 'd' is the first item in the string:
-            // 1a) if the only thing after it is a positive number (X):    dX
-              // roll once with (X) many sides, return total
-            // 1b) if the thing after it is not a positve number
-              // return an ERROR
       // 2) if 'd' is to the right of a positive number (N)
             // 2a) if the only thing to the right of 'd' is a positive number (X):    NdX
               // roll N dice with X sides, return total
@@ -58,7 +62,7 @@ router.get('/:roll', function(req, res, next) {
   function isXAnyPositiveNumber(X) {
     if (Math.floor(Number(X)) == X){
       if (0 < X) {
-        return true;
+        return Math.floor(Number(X));
       }
     }
     return false;
@@ -71,8 +75,12 @@ router.get('/:roll', function(req, res, next) {
 });
 
 function rollOneDi(nSides) {
+  console.log("rolling di with",nSides," sides");
   let randomFactor = Math.random();
   console.log("random factor:",randomFactor);
+  let result = Math.floor(randomFactor*nSides)+1
+  console.log("result:",result);
+  return result
 }
 
 module.exports = router;
